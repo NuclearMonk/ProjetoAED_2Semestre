8af066de270c325fcc6d1d5b2534eed17bac7156
MEEC PROG 1920 <student@localhost.localdomain>
2021-03-15 20:00:58 +0000

    stack list 1.1

--- stack.c
+++ stack.c
@@ -1,11 +1,13 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include "stack.h"
-
+#include "slist.h"
+/*
 struct _stack{
 	unsigned int stpointer;
 	Data*	array;
 };
+
 
 
 t_stack* ST_New(unsigned int sz){
@@ -41,48 +43,49 @@ int ST_Used(t_stack* st){
 bool ST_IsEmpty(t_stack* st){
 	return (st->stpointer == 0);
 }
+*/
 
-/*Typedef struct _stack{
-	Unsigned int length;
-	t_slist* 	lista;
+struct _stack{
+	unsigned int length;
+	t_slist*	lista;
 };
 
-stack* ST_New(unsigned int sz){
-	St = malloc(sz*sizeof(t_slist));
-	St.length =0;
-	Lista = SL_Initialize();
+t_stack* ST_New(unsigned int sz){
+	t_stack* new_stack = malloc(sz*sizeof(t_stack));
+	new_stack->length = 0;
+    new_stack->lista = SL_Initialize();
+	return new_stack;
 }
 
-Void 	ST_Free(stack* st){
+void 	ST_Free(t_stack* st){
+	free(st->lista);
 	free(st);
 }
 
-Void 	ST_Push(DATA data , stack* st){
-	Slist * aux = SL_NewElement(data);
-	st.lista = SL_Prepend(st.lista, aux);
-	st.length ++;
+void 	ST_Push(t_stack* st, Data data){
+	t_slist* aux = SL_NewElement(data);
+	st->lista = SL_Prepend(st->lista, aux);
+	st->length ++;
 }
 
-DATA  ST_Pop(stack* st){
-	DATA Aux = SL_GetData(lista);
-	Slist aux2= lista;
-	Lista = SL_GetNext(lista);
-	SL_FreeElement(aux2,&DoNothing());
-	St.legth--;
-    Return Aux;
+Data  ST_Pop(t_stack* st){
+	Data Aux = SL_GetData(st->lista);
+    t_slist* aux2= st->lista;
+	st->lista = SL_GetNext(st->lista);
+	SL_FreeElement(aux2,&DoNothing);
+	st->length--;
+    return Aux;
 }
 
-DATA  ST_Peek(stack* st){
-	Return SL_GetData(st.lista);
+Data  ST_Peek(t_stack* st){
+	return SL_GetData(st->lista);
 }
 
 
-Int 	ST_Used(stack* st){
-	Return st.leght;
+int 	ST_Used(t_stack* st){
+	return st->length;
 }
 
-Bool 	ST_IsEmpty(stack* st){
-	Return (st.leght == 0);
+bool 	ST_IsEmpty(t_stack* st){
+	return (st->length == 0);
 }
-
-*/
--- stack.h
+++ stack.h
@@ -3,7 +3,7 @@
 #include "Data.h"
 
 typedef struct _stack t_stack;
-
+/*
 t_stack* ST_New(unsigned int sz);
 void ST_Free(t_stack* st);
 void ST_Push(t_stack* st,Data data);
@@ -11,12 +11,12 @@ Data  ST_Pop(t_stack* st);
 Data  ST_Peek(t_stack* st);
 int ST_Used(t_stack* st);
 bool ST_IsEmpty(t_stack* st);
-
-/*
+*/
+void    DoNothing(Data data);
 void 	ST_Free(t_stack* st);
-void 	ST_Push(DATA data , t_stack* st);
+void 	ST_Push(t_stack* st, Data data);
 Data    ST_Pop(t_stack* st);
 Data    ST_Peek(t_stack* st);
 int 	ST_Used(t_stack* st);
 bool 	ST_IsEmpty(t_stack* st);
-*/
+

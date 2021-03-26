5b387cd03e78b0d35c53eef667dccfa951492b32
MEEC PROG 1920 <student@localhost.localdomain>
2021-03-26 19:37:59 +0000

    ler prob 1.1

--- datacollect.c
+++ datacollect.c
@@ -1,2 +1,32 @@
 #include "datacolect.h"
 #include <stdio.h>
+
+void le_cabecalho_prob(FILE *fpprob, char *cabeca){
+    if (fscanf(fpprob, "%s", cabeca) != 1){
+        exit(0);
+    }
+    else if (strcmp(cabeca, "A0") == 0)
+    {
+         if (fscanf(fpprob, "%c", cabeca[2]) != 1){
+        exit(0);
+        }
+    }
+    else if (strcmp(cabeca, "B0") == 0)
+    {
+        if (fscanf(fpprob, "%c %c", cabeca[2], cabeca[3]) != 1){
+        exit(0);
+        }
+    }
+    else if (strcmp(cabeca, "C0") == 0)
+    {
+        if (fscanf(fpprob, "%c %c", cabeca[2], cabeca[3]) != 1){
+        exit(0);
+        }
+    }
+    else if (strcmp(cabeca, "D0") == 0)
+    {
+        if (fscanf(fpprob, "%c %c", cabeca[2], cabeca[3]) != 1){
+        exit(0);
+        }
+    }
+}

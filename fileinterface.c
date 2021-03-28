d7bffc41e6980ffe4152da715d776389b9296e04
ManuelSoares <=>
2021-03-28 15:48:24 +0100

    fixes

--- fileinterface.c
+++ fileinterface.c
@@ -7,7 +7,7 @@
 
 /*Modificar, le para variavel auxiliar temporaria e depois escrever para um cabecalho alocado usando o struct cabecalho e retornar o cabecalho ver warnings compilacao*/
 /*Ja agora mesmo que n usasses isso cabecalho deveria ser um array de ints onde passavamos a informacao correta*/
-void le_cabecalho_prob(FILE *fpprob, char *cabeca){
+void le_cabecalho_prob(FILE *fpprob, char *cabeca){  /*Vai ter que retornar cabecalho*/
     if (fscanf(fpprob, "%s", cabeca) != 1){
         exit(0);
     }

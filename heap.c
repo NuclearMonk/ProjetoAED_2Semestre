07818b8f84a8453a600f1aa516f34b0132f0da34
ManuelSoares <=>
2021-05-08 17:36:38 +0100

    Code cleanup

--- heap.c
+++ heap.c
@@ -11,8 +11,8 @@ struct _heap{
     int  (*_CompareFunction)(int keyA,int keyB);
 };
 
-void        PQ_FixUp(heap_t* heap, int index);
-void        PQ_FixDown(heap_t* heap, int index);
+void        HP_FixUp(heap_t* heap, int index);
+void        HP_FixDown(heap_t* heap, int index);
 
 
 heap_t*   HP_Alocar(int maxsize,int  (*CompareFunction)(int keyA,int keyB)){
@@ -32,20 +32,20 @@ void        HP_Libertar(heap_t* heap){
 
 
 void        PQ_FixUp(heap_t* heap, int index){
-    while(index>0 && heap->_CompareFunction(heap->_array[index],heap->_array[(index-1)/2])){ /*Iterativamente troca cada dilho com o seu pai se o pai tiver pior prioridade que o filho*/
+    while(index>0 && heap->_CompareFunction(heap->_array[index],heap->_array[(index-1)/2])){ /*Iterativamente troca cada filho com o seu pai se o pai tiver pior prioridade que o filho*/
         SWAP(index, (index-1)/2);
         index = (index-1/2);
     }
 }
 
-void        PQ_FixDown(heap_t* heap, int index){
+void        HP_FixDown(heap_t* heap, int index){
     int child=0;
     /*Iterativamente trocamos cada pai com o pior dos seus filhos se o pai for pior que um dos filhos
       fica implicito que se apenas um dos filhos for pior que o pai esse filho vai ser o pior dos filhos*/
     while(2*index< heap->_size-1){
         child = 2*index+1;
         if(child<heap->_size-1&& heap->_CompareFunction(heap->_array[child],heap->_array[child+1]))child++; /*Se existem duas folhas comparamos definimos como child a folha com pior prioridade*/
-        if(heap->_CompareFunction(heap->_array[index],heap->_array[child]))break; /*Verificada a condicao de acerco, logo podemos sair*/
+        if(heap->_CompareFunction(heap->_array[index],heap->_array[child]))break; /*Verificada a condicao de acervo, logo podemos sair*/
         SWAP(index, child);
         index=child;
     }
